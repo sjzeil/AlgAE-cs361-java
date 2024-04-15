fb821c66b4c195f9b9890c8d0ff07ea23cbdafc8
Steven Zeil <zeil@cs.odu.edu>
2024-04-14 20:58:22 -0400

    arrayList demo: add to end

---
+++ arrayList/src/main/java/edu/odu/cs/cs361/animations/ArrayListAnimation.java
@@ -0,0 +1,107 @@
+package edu.odu.cs.cs361.animations;
+
+
+
+import java.awt.Color;
+import java.util.List;
+
+import edu.odu.cs.AlgAE.Animations.LocalJavaAnimation;
+import edu.odu.cs.AlgAE.Common.Snapshot.Entity.Directions;
+import edu.odu.cs.AlgAE.Server.MenuFunction;
+import edu.odu.cs.AlgAE.Server.MemoryModel.Component;
+import edu.odu.cs.AlgAE.Server.MemoryModel.Connection;
+import edu.odu.cs.AlgAE.Server.Rendering.Renderer;
+import edu.odu.cs.AlgAE.Server.Utilities.SimpleReference;
+
+
+public class ArrayListAnimation extends LocalJavaAnimation {
+
+	public ArrayListAnimation() {
+		super("Array Operations");
+	}
+
+	@Override
+	public String about() {
+		return "Demonstration of ArrayList implementation algorithms,\n" +
+		"prepared for CS 361, Advanced Data Structures\n" +
+		"and Algorithms, Old Dominion University\n" +
+		"Summer 2024";
+	}
+
+	private ArrayList<Integer> a = new ArrayList<Integer>();
+	
+	
+	
+	
+	@Override
+	public void buildMenu() {
+		
+		
+		
+		registerStartingAction(new MenuFunction() {
+			
+			@Override
+			public void selected() {
+				generateRandomVector(3);
+				globalVar("a", a);
+			}
+		});
+		
+		register ("Generate an ArrayList", new MenuFunction() {
+			@Override
+			public void selected() {
+				randomVectorGenerated();
+			}
+		});
+
+				
+		register ("a.add(...)", new MenuFunction() {
+			@Override
+			public void selected() {
+				String value = promptForInput("Value to add:", "[0-9]+");
+				try {
+					Integer v = Integer.parseInt(value);
+					a.add (v.intValue());
+				} catch (Exception e) {
+					// do nothing
+				}
+			}
+		});
+
+
+/*
+		register ("a.clear()", new MenuFunction() {
+			@Override
+			public void selected() {
+				a.clear();
+			}
+		});
+
+*/
+
+    }
+	
+	public void randomVectorGenerated()
+	{
+		String value = promptForInput("How many elements?", "\\d+");
+		int n = Integer.parseInt(value);
+		generateRandomVector(n);
+	}
+
+	public void generateRandomVector(int n)
+	{
+		a.quick_clear();
+		for (int i = 0; i < n; ++i)
+			a.quick_add ((int)(25.0*Math.random()));
+	}
+
+	
+	
+	public static void main (String[] args) {
+		ArrayListAnimation demo = new ArrayListAnimation();
+		demo.runAsMain();
+	}
+	
+
+
+}
